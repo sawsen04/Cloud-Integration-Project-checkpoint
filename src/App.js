d8dc80054da2166a07f2cde0e7f9719a1e7fa4e0
sawsen04 <sawsenayari87@gmail.com>
2024-01-10 01:42:46 +0100

    final

--- src/App.js
+++ src/App.js
@@ -4,23 +4,20 @@ import "leaflet-control-geocoder/dist/Control.Geocoder.css";
 import "leaflet-control-geocoder/dist/Control.Geocoder.js";
 import "./App.css";
 import LeafletRoutingMachine from "./LeafletRoutingMachine";
-
 import SecTwo from "./components/section-two/SecTwo";
-
-//import LeafletGeoCoder from "./LeafletGeoCoder";
+import LeafletGeoCoder from "./LeafletGeoCoder";
 
 function App() {
   const position = [36.81202966835531, 10.143146261373557];
   return (
     <div className="App">
-      
       <SecTwo />
       <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {/* <LeafletGeoCoder />*/}
+        <LeafletGeoCoder />
         <LeafletRoutingMachine />
       </MapContainer>
     </div>
